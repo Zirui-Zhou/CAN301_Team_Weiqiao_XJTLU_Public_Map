2bb4d95ec7b35459672b227051dfe77af95688fc
henryyou614 <114057268+henryyou614@users.noreply.github.com>
2022-11-28 16:21:26 +0800

    change the location of popwindow

--- can301-android/app/src/main/java/com/example/can301_2/MainActivity.java
+++ can301-android/app/src/main/java/com/example/can301_2/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity{
 
     private final String TAG = "MainActivity";
     private ActivityMainBinding binding;
+    private BottomNavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity{
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -94,6 +95,10 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public boolean onSupportNavigateUp() {
         return Navigation.findNavController(this, R.id.nav_host_fragment_activity_main).navigateUp();
+    }
+    
+    public int getBarHeight() {
+        return navView.getMeasuredHeight();
     }
 
 }
